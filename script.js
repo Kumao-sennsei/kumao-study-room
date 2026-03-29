a36c36a54c74013c458c7655ac47cfcc53f92188
Kumao-sennsei <gooday.jukucafe@gmail.com>
2026-03-29 21:30:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1035,7 +1035,7 @@ function renderStoryBook() {
     html += `
       <div class="story-row">
         <div class="story-month">${month}月</div>
-        <div class="story-line" ${isUnlocked ? `onclick="playStoryAudio(${month})"` : ""}>${text}</div>
+        <div class="story-line" data-month="${month}" ${isUnlocked ? `onclick="playStoryAudio(${month})"` : ""}>${text}</div>
       </div>
     `;
   });
