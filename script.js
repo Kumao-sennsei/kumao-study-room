4bb395436789c815b5524070348ef8304a76072b
Kumao-sennsei <gooday.jukucafe@gmail.com>
2026-04-15 20:45:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1173,6 +1173,22 @@ async function startStudy(mode) {
 // 初期化
 showHomeUI();
 window.startStudy = startStudy;
+window.addEventListener(
+  "touchstart",
+  () => {
+    unlockAudioSystem().catch(() => {});
+  },
+  { once: true, passive: true }
+);
+
+window.addEventListener(
+  "click",
+  () => {
+    unlockAudioSystem().catch(() => {});
+  },
+  { once: true, passive: true }
+);
+
 
 window.addEventListener(
   "touchstart",
