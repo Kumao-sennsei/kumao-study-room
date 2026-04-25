611ec8930aafdad27d35313f8b04fd800b44c763
Kumao-sennsei <gooday.jukucafe@gmail.com>
2026-04-26 00:23:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -727,7 +727,7 @@ function getCurrentMonth() {
     month: "numeric"
   }).format(now);
 console.log("[month] monthText =", monthText, "Number(monthText) =", Number(monthText));
-    return 6;
+    return 9;
 }
 
 function pickByWeight(items) {
