8481473e3bf827b1ed7c689f16815104e8fd7711
Kumao-sennsei <gooday.jukucafe@gmail.com>
2026-07-05 00:41:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1161,8 +1161,8 @@ function getAvailableRareQuotesForCurrentMonth() {
 // ======================
 // 設定・状態管理
 // ======================
-const FOCUS_SEC = 0.3 * 60;
-const BREAK_SEC = 0.3 * 60;
+const FOCUS_SEC = 0.5 * 60;
+const BREAK_SEC = 0.5 * 60;
 const SETS_PER_ROUND = 4;
 
 let currentMode = "";
